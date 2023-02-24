0cbb31de92f68855924615eee247fe6ddefe66f9
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-02-24 12:07:45 +0100

    Update broker_functions.py

--- broker_functions.py
+++ broker_functions.py
@@ -1,1 +1,10 @@
+import logging
+
+
+class BrokerFunctions:
+    def __init__(self):
+        self.logger = logging.getLogger(__name__)
+
+    def run(self):
+        self.logger.info("Broker functions selected.")
 
