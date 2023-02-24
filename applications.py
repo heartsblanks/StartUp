c934906393edef055aa6cc811eed0aa7c58025ee
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-02-24 13:18:51 +0100

    Update applications.py

--- applications.py
+++ applications.py
@@ -1,47 +1,63 @@
 import tkinter as tk
 from tkinter import ttk
+import logging
+
 from application_treeview import ApplicationTreeview
 
 
 class Applications:
     def __init__(self):
-        self.applications = []
-        self.app_treeview = ApplicationTreeview()
+        self.treeview = None
 
     def run(self):
         try:
             # Create new window
-            self.app_window = tk.Toplevel()
-            self.app_window.title("Applications")
-            self.app_window.geometry("600x400")
+            app_window = tk.Toplevel()
+            app_window.title("Applications")
+            app_window.geometry("600x400")
 
             # Add scrollbar
-            scrollbar = ttk.Scrollbar(self.app_window)
+            scrollbar = ttk.Scrollbar(app_window)
             scrollbar.pack(side="right", fill="y")
 
-            # Add treeview
-            self.treeview = self.app_treeview.create(self.app_window, scrollbar)
+            # Create Application Treeview
+            self.treeview = ApplicationTreeview()
+            self.treeview.create(app_window, scrollbar)
+
+            # Load applications from JSON
+            self.treeview.load_applications()
 
             # Add "Open" button
-            open_button = ttk.Button(self.app_window, text="Open", command=self.open_selected)
+            open_button = ttk.Button(app_window, text="Open", command=self.open_selected)
             open_button.pack(padx=10, pady=5, side="bottom")
+
         except Exception as e:
-            print(f"An error occurred while running the Applications class: {e}")
+            logging.error(f"An error occurred while running the Applications class: {e}")
 
     def open_selected(self):
         try:
             # Get selected items
-            items = self.treeview.selection()
+            items = self.treeview.treeview.selection()
 
             # Get locations of selected applications
             locations = []
             for item in items:
-                location = self.app_treeview.get_location(item)
-                locations.append(location)
+                location = self.treeview.get_location(item)
+                if location:
+                    locations.append(location)
 
             # Open selected applications
             for location in locations:
-                # Do something with the location
-                print(location)
+                self.open_application(location)
+
         except Exception as e:
-            print(f"An error occurred while opening the application: {e}")
+            logging.error(f"An error occurred while opening the selected applications: {e}")
+
+    def open_application(self, location):
+        try:
+            app_window = tk.Toplevel()
+            app_window.title("Application")
+            app_window.geometry("600x400")
+            # Do something with the location
+        except Exception as e:
+            logging.error(f"An error occurred while opening the application: {e}")
