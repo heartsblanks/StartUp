ae6c931c569df520b8ca94f77a4a0dbbb2eb3676
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-02-24 12:08:03 +0100

    Update favourites.py

--- favourites.py
+++ favourites.py
@@ -1,1 +1,10 @@
+import logging
+
+
+class Favourites:
+    def __init__(self):
+        self.logger = logging.getLogger(__name__)
+
+    def run(self):
+        self.logger.info("Favourites selected.")
 
