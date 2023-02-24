1885feda15ff800653de9eaf279f007029150acb
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-02-24 12:06:15 +0100

    Update add_items.py

--- add_items.py
+++ add_items.py
@@ -1,1 +1,10 @@
+import logging
+
+
+class AddItems:
+    def __init__(self):
+        self.logger = logging.getLogger(__name__)
+
+    def run(self):
+        self.logger.info("Add items selected.")
 
