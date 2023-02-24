7dad01b863c7f64622a4743f5be5984f184ac1e6
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-02-25 00:49:46 +0100

    Revert "Update favourites.py"
    This reverts commit 97c0825b88761e0881897f4ec7786abe95d09736.

--- favourites.py
+++ favourites.py
@@ -1,37 +1,68 @@
-import json
-import logging
-import subprocess
 import tkinter as tk
-from tkinter import ttk, messagebox
+from tkinter import ttk
+import json
+import subprocess
+from tkinter import messagebox
+
 from favourites_treeview import FavouritesTreeview
-from window_base import BaseWindow
-
-
-class FavouritesWindow(BaseWindow):
-    def __init__(self, master):
-        super().__init__(master, "Favourites")
-
-        # Add favourites treeview
-        self.favourites_treeview = FavouritesTreeview(self.window_frame)
-        self.favourites_treeview.treeview.pack(fill="both", padx=10, pady=10, expand=True)
-
-        # Add "Open" button
-        open_button = ttk.Button(self.window_frame, text="Open", command=self.favourites_treeview.open_selected)
-        open_button.pack(padx=10, pady=5, side="bottom")
-
-        # Add "Quit" button
-        quit_button = ttk.Button(self.window_frame, text="Quit", command=self.close_window)
-        quit_button.pack(padx=10, pady=5, side="bottom")
 
 
 class Favourites:
     def __init__(self, favourites_frame):
         self.favourites_frame = favourites_frame
+        self.favourites_vars = []
+        self.favourites_treeview = None
+
+    def create_check_buttons(self):
+        try:
+            with open("Constants.json") as f:
+                data = json.load(f)
+                favourites = data["Favourites"]
+                for fav in favourites:
+                    fav_label = fav["Name"]
+                    category_frame = ttk.LabelFrame(self.favourites_frame, text=fav_label)
+                    category_frame.pack(fill="both", padx=10, pady=5, expand=True)
+                    for item in fav["Items"]:
+                        var = tk.BooleanVar()
+                        item_name = item["Name"]
+                        check_button = ttk.Checkbutton(category_frame, text=item_name, variable=var)
+                        check_button.pack(fill="x", padx=10, pady=5)
+                        self.favourites_vars.append(var)
+        except Exception as e:
+            print(f"An error occurred while creating the favourites checkbuttons: {e}")
+
+    def get_selected(self):
+        selected = [i for i, v in enumerate(self.favourites_vars) if v.get()]
+        return selected
 
     def open_favourites(self):
         try:
+            # Get the selected favourites
+            selected = self.get_selected()
+            if not selected:
+                messagebox.showinfo("Error", "Please select at least one favourite to open.")
+                return
+
             # Create favourites window
-            favourites_window = FavouritesWindow(self.favourites_frame)
+            favourites_window = tk.Toplevel(self.favourites_frame.master)
+            favourites_window.title("Favourites")
+            favourites_window.geometry("600x400")
+
+            # Add scrollbar
+            scrollbar = ttk.Scrollbar(favourites_window)
+            scrollbar.pack(side="right", fill="y")
+
+            # Add favourites treeview
+            self.favourites_treeview = FavouritesTreeview(favourites_window)
+            self.favourites_treeview.create(self.favourites_treeview.treeview)
+
+            # Add "Open" button
+            open_button = ttk.Button(favourites_window, text="Open", command=self.favourites_treeview.open_selected)
+            open_button.pack(padx=10, pady=5, side="bottom")
+
+            # Add "Quit" button
+            quit_button = ttk.Button(favourites_window, text="Quit", command=favourites_window.destroy)
+            quit_button.pack(padx=10, pady=5, side="bottom")
 
         except Exception as e:
-            logging.error(f"An error occurred while opening the favourites window: {e}")
+            print(f"An error occurred while opening the selected favourites: {e}")
