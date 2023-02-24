123f09aee4a86bd07bfd7e367a7eb0b5c7f62c7c
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-02-24 16:26:12 +0100

    Update favourites.py

--- favourites.py
+++ favourites.py
@@ -4,25 +4,28 @@ from tkinter import ttk
 
 
 class Favourites:
-    def __init__(self, favourites_frame):
-        self.favourites = []
+    def __init__(self, frame):
+        self.frame = frame
         self.favourites_vars = []
-        self.favourites_frame = favourites_frame
+        self.load_favourites()
 
     def create_check_buttons(self):
         try:
+            for fav in self.favourites:
+                var = tk.BooleanVar()
+                check_button = ttk.Checkbutton(self.frame, text=fav["Name"], variable=var)
+                check_button.pack(fill="x", padx=10, pady=5)
+                self.favourites_vars.append(var)
+        except Exception as e:
+            print(f"An error occurred while creating the favourites buttons: {e}")
+
+    def load_favourites(self):
+        try:
             with open("Constants.json") as f:
                 data = json.load(f)
-                favourites = data["Favourites"]
-                for fav in favourites:
-                    var = tk.BooleanVar()
-                    check_button = ttk.Checkbutton(self.favourites_frame, text=fav["Name"], variable=var)
-                    check_button.pack(fill="x", padx=10, pady=5)
-                    self.favourites_vars.append(var)
-                    self.favourites.append({"name": fav["Name"], "location": fav["Location"]})
+                self.favourites = data["Favourites"]
         except Exception as e:
-            print(f"An error occurred while creating the favourites check buttons: {e}")
+            print(f"An error occurred while loading the favourites: {e}")
 
     def get_selected(self):
-        selected = [fav for fav, var in zip(self.favourites, self.favourites_vars) if var.get()]
-        return selected
+        return [i for i, v in enumerate(self.favourites_vars) if v.get()]
