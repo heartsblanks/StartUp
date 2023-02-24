f4b76dd51e4c8fe89915fc4ff038c756878572b7
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-02-25 00:29:34 +0100

    Update applications.py

--- applications.py
+++ applications.py
@@ -1,77 +1,78 @@
-import tkinter as tk
-from tkinter import ttk
+import json
 import logging
 import subprocess
+import tkinter as tk
+from tkinter import ttk, messagebox
 
 from application_treeview import ApplicationTreeview
+from application_window import ApplicationsWindow
+from window_base import WindowBase
+from add_items import AddItems
 
 
-class Applications:
-    def __init__(self):
-        self.treeview = None
+class Applications(WindowBase):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.treeview = ApplicationTreeview(self.main_frame)
+        self.treeview.pack(fill="both", expand=True)
 
-    def run(self):
-        try:
-            # Create new window
-            app_window = tk.Toplevel()
-            app_window.title("Applications")
-            app_window.geometry("600x400")
+        self.create_menu()
+        self.create_toolbar()
 
-            # Add scrollbar
-            scrollbar = ttk.Scrollbar(app_window)
-            scrollbar.pack(side="right", fill="y")
+    def create_menu(self):
+        menu_bar = tk.Menu(self.parent)
+        self.parent.config(menu=menu_bar)
 
-            # Create Application Treeview
-            self.treeview = ApplicationTreeview()
-            self.treeview.create(app_window, scrollbar)
+        file_menu = tk.Menu(menu_bar, tearoff=False)
+        menu_bar.add_cascade(label="File", menu=file_menu)
+        file_menu.add_command(label="Quit", command=self.parent.quit)
 
-            # Add "Open" button
-            open_button = ttk.Button(app_window, text="Open", command=self.open_selected)
-            open_button.pack(padx=10, pady=5, side="bottom")
+        edit_menu = tk.Menu(menu_bar, tearoff=False)
+        menu_bar.add_cascade(label="Edit", menu=edit_menu)
+        edit_menu.add_command(label="Add Application", command=self.add_application)
 
-            # Add "Quit" button
-            quit_button = ttk.Button(app_window, text="Quit", command=app_window.destroy)
-            quit_button.pack(padx=10, pady=5, side="bottom")
+    def create_toolbar(self):
+        toolbar = ttk.Frame(self.parent, padding=5)
+        toolbar.pack(side="top", fill="x")
 
-        except Exception as e:
-            logging.error(f"An error occurred while running the Applications class: {e}")
+        add_button = ttk.Button(toolbar, text="Add", command=self.add_application)
+        add_button.pack(side="left")
 
-    def open_selected(self):
-        try:
-            # Get selected items
-            items = self.treeview.treeview.selection()
+        delete_button = ttk.Button(toolbar, text="Delete", command=self.delete_selected)
+        delete_button.pack(side="left", padx=5)
 
-            if not items:
-                # Show error message if no item is selected
-                tk.messagebox.showerror("Error", "Please select at least one application to open.")
-                return
+        launch_button = ttk.Button(toolbar, text="Launch", command=self.launch_selected)
+        launch_button.pack(side="left")
 
-            # Get locations of selected applications
-            locations = []
-            for item in items:
-                location = self.treeview.get_location(item)
-                if location:
-                    locations.append(location)
+    def add_application(self):
+        AddItems(self.treeview, "Applications")
 
-            # Open selected applications
-            for location in locations:
-                subprocess.Popen(location)
+    def delete_selected(self):
+        self.treeview.delete_selected()
 
-        except Exception as e:
-            logging.error(f"An error occurred while opening the selected applications: {e}")
+    def launch_selected(self):
+        selected = self.treeview.selection()
 
-    def run_from_favourites(self, items):
-        try:
-            # Get locations of selected applications
-            locations = []
-            for item in items:
-                location = item["Location"]
-                if location:
-                    locations.append(location)
+        if not selected:
+            messagebox.showinfo("Error", "Please select at least one application to launch.")
+            return
 
-            # Open selected applications
-            for location in locations:
-                subprocess.Popen(location)
+        for item in selected:
+            location = self.treeview.get_location(item)
+            if location:
+                try:
+                    subprocess.Popen(location)
+                except Exception as e:
+                    logging.error(f"An error occurred while opening the selected application: {e}")
+            else:
+                logging.error(f"Location not found for selected application: {item}")
 
-        except Exception as e:
-            logging.error(f"An error occurred while opening the selected applications: {e}")
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title("Applications")
+    root.geometry("800x600")
+
+    app = Applications(root)
+
+    root.mainloop()
