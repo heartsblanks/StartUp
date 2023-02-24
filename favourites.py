8ed87556180b685ddbaf1a8e4ef96f7ce4d4364d
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-02-24 16:08:34 +0100

    Update favourites.py

--- favourites.py
+++ favourites.py
@@ -1,4 +1,6 @@
 import logging
+from tkinter import ttk
+from favourites_treeview import FavouritesTreeview
 
 
 class Favourites:
@@ -8,3 +10,22 @@ class Favourites:
     def run(self):
         self.logger.info("Favourites selected.")
 
+        # Create favourites window
+        favourites_window = tk.Toplevel(self.master)
+        favourites_window.title("Favourites")
+        favourites_window.geometry("600x400")
+
+        # Add scrollbar
+        scrollbar = ttk.Scrollbar(favourites_window)
+        scrollbar.pack()
+        # Add favourites treeview
+        favourites_treeview = FavouritesTreeview()
+        favourites_treeview.create(favourites_window, scrollbar)
+
+        # Add "Open" button
+        open_button = ttk.Button(favourites_window, text="Open", command=favourites_treeview.open_selected)
+        open_button.pack(padx=10, pady=5, side="bottom")
+
+        # Add "Quit" button
+        quit_button = ttk.Button(favourites_window, text="Quit", command=favourites_window.destroy)
+        quit_button.pack(padx=10, pady=5, side="bottom")
