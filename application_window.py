196108959a2569798aa5d7eb4b77a1cacd9f9f99
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-02-25 00:49:52 +0100

    Revert "Update application_window.py"
    This reverts commit fff39ad27ae8dd897e5000dda7b96d6dfefcd81c.

--- application_window.py
+++ application_window.py
@@ -1,60 +1,26 @@
-import json
-import logging
-import subprocess
 import tkinter as tk
 from tkinter import ttk
+import json
 
-from window_base import WindowBase
+from applications import Applications
 
 
-class ApplicationsWindow(WindowBase):
+class ApplicationWindow:
     def __init__(self):
-        super().__init__("Applications")
+        self.favourites_data = None
+        self.applications = Applications()
 
-    def create_form(self):
-        super().create_form()
-
-        location_label = ttk.Label(self.form_frame, text="Location:")
-        self.location_entry = ttk.Entry(self.form_frame)
-        location_label.grid(row=2, column=0, sticky="W")
-        self.location_entry.grid(row=2, column=1, sticky="W")
-
-    def create_buttons(self):
-        super().create_buttons()
-
-        self.add_items_button = ttk.Button(self.button_frame, text="Add Items", command=self.add_items)
-        self.add_items_button.pack(side="left", padx=5)
-
-    def add_items(self):
-        from add_items import AddItems
-
-        add_items_window = AddItems()
-        add_items_window.category_name = "Applications"
-        add_items_window.run()
-
-    def load_items(self):
+    def create(self, master):
         try:
+            # Load favourites data from Constants.json
             with open("Constants.json") as f:
                 data = json.load(f)
-                applications = data["Applications"]
+                self.favourites_data = data.get("Favourites", {})
 
-                for application in applications:
-                    name = application["Name"]
-                    location = application["Location"]
-                    self.treeview.insert("", "end", text=name, values=(location,))
-
-        except Exception as e:
-            logging.error(f"An error occurred while loading the applications: {e}")
-
-    def open_selected(self):
-        try:
-            # Get selected item
-            item = self.treeview.selection()[0]
-            name = self.treeview.item(item, "text")
-            location = self.treeview.item(item, "values")[0]
-
-            # Open selected application
-            subprocess.Popen(location)
+            # Create a button for each category
+            for category, items in self.favourites_data.items():
+                button = ttk.Button(master, text=category, command=lambda items=items: self.applications.run_from_favourites(items))
+                button.pack(fill="x", padx=10, pady=5)
 
         except Exception as e:
-            logging.error(f"An error occurred while opening the selected application: {e}")
+            print(f"An error occurred while creating the Applications button: {e}")
