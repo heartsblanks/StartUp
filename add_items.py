0c5e02a630c1c226094846ccb0b79d39938887bc
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-02-25 00:50:06 +0100

    Revert "Update add_items.py"
    This reverts commit 9811cbecb8a3958df59bdfc7dadb0c3786914f4f.

--- add_items.py
+++ add_items.py
@@ -1,11 +1,9 @@
-import json
 import tkinter as tk
 from tkinter import ttk
 
 
 class AddItems:
-    def __init__(self, category_name):
-        self.category_name = category_name
+    def __init__(self):
         self.window = tk.Toplevel()
         self.window.title("Add new item")
 
@@ -30,11 +28,29 @@ class AddItems:
         button_frame = ttk.Frame(self.window, padding=20)
         button_frame.pack()
 
+        apps_button = ttk.Button(button_frame, text="Applications", command=self.add_application)
+        apps_button.pack(side="left", padx=5)
+
+        websites_button = ttk.Button(button_frame, text="Websites", command=self.add_website)
+        websites_button.pack(side="left", padx=5)
+
         submit_button = ttk.Button(button_frame, text="Submit", command=self.submit)
         submit_button.pack(side="left", padx=5)
 
         quit_button = ttk.Button(button_frame, text="Quit", command=self.window.destroy)
         quit_button.pack(side="left", padx=5)
+
+    def add_application(self):
+        self.location_entry.config(state="normal")
+        self.location_entry.delete(0, tk.END)
+        self.location_entry.insert(0, "C:/Program Files/Application")
+        self.location_entry.config(state="readonly")
+
+    def add_website(self):
+        self.location_entry.config(state="normal")
+        self.location_entry.delete(0, tk.END)
+        self.location_entry.insert(0, "https://example.com")
+        self.location_entry.config(state="normal")
 
     def submit(self):
         name = self.name_entry.get()
