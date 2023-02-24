38d4021c465330a207ae57bdfcf35c6cea68c0a8
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-02-24 12:05:32 +0100

    Update check_json.py

--- check_json.py
+++ check_json.py
@@ -1,1 +1,17 @@
+import json
+import logging
+
+
+class JSONValidator:
+    def __init__(self, json_file):
+        self.json_file = json_file
+        self.logger = logging.getLogger(__name__)
+
+    def validate(self):
+        try:
+            with open(self.json_file) as f:
+                json.load(f)
+        except Exception as e:
+            self.logger.error(f"Error validating JSON file: {e}")
+            raise e
 
