a2608139646fe9c739936f6bcd4d150e3b0b56ee
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-02-24 12:05:58 +0100

    Update websites.py

--- websites.py
+++ websites.py
@@ -1,1 +1,10 @@
+import logging
+
+
+class Websites:
+    def __init__(self):
+        self.logger = logging.getLogger(__name__)
+
+    def run(self):
+        self.logger.info("Websites selected.")
 
