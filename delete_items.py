bca7c9d1eb9f2d8b4fec38b8543b366090a7b5d6
heartsblanks <125401145+heartsblanks@users.noreply.github.com>
2023-02-24 12:07:31 +0100

    Update delete_items.py

--- delete_items.py
+++ delete_items.py
@@ -1,1 +1,10 @@
+import logging
+
+
+class DeleteItems:
+    def __init__(self):
+        self.logger = logging.getLogger(__name__)
+
+    def run(self):
+        self.logger.info("Delete items selected.")
 
